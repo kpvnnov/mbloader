0b891e48a740a7dcb458cefd56dc1a9636a056d7
kpv <kpvnnov@gmail.com>
2024-11-29 22:46:22 +0300

    добавил в программу блок объединения двух файлов - бутлоадера и программы под бутлоадер с автозаполнениями полей контрольной суммы, длины программы

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,15 @@
 #include <qtimer.h>
 #include <QMetaEnum>
 #include <QLoggingCategory>
+#include <QFileInfo>
+
+//#define FLASH_APP_START_ADDR (FLASH_BASE + FLASH_PAGE_SIZE)
+//для CC версии прошивка начинается с этого адреса
+#define FLASH_APP_START_ADDR 0x800
+#define SERIAL_NUM_IN_BOOT (50*4)
+#define CRC_MAIN_PROGRAM ((49-1)*4)
+#define LENGTH_MAIN_PROGRAM ((50-1)*4)
+
 
 using namespace Qt::Literals::StringLiterals;
 
@@ -73,11 +82,22 @@ int main(int argc, char *argv[])
     QFile fileMainload;
     QString errorStr;
 
+      auto MainloadFile{ positionalArguments.at(0) };
       auto bootloaderFile{ positionalArguments.at(1) };
-    fileBootload.setFileName(bootloaderFile);
 
+    fileMainload.setFileName(MainloadFile);
+    fileBootload.setFileName(bootloaderFile);
     if (!fileBootload.open(QIODeviceBase::ReadOnly)) {
         errorStr = fileBootload.errorString();
+        cerr << u"Error open bootloader file\n"_s << errorStr<< Qt::endl;
+        //emit finished(false);
+        //return;
+        parser.showHelp(1);
+
+    }
+    if (!fileMainload.open(QIODeviceBase::ReadOnly)) {
+        errorStr = fileMainload.errorString();
+        cerr << u"Error open file main program\n"_s << errorStr<< Qt::endl;
         //emit finished(false);
         //return;
         parser.showHelp(1);
@@ -87,22 +107,105 @@ int main(int argc, char *argv[])
     char bootloaderTemp[262144]; //256K
     char programTemp[262144]; //256K
 
-        QByteArray byteArray;
-        QDataStream dataStream{ &byteArray, QIODeviceBase::WriteOnly };
 
-        auto readCount{ fileBootload.read(bootloaderTemp, sizeof(bootloaderTemp)) };
-        if (readCount == -1) {
+        auto readCountBoot{ fileBootload.read(bootloaderTemp, sizeof(bootloaderTemp)) };
+        if (readCountBoot == -1) {
             errorStr = fileBootload.errorString();
             //emit finished(false);
             //return;
-        parser.showHelp(1);
+        cerr << u"Error open bootload \n"_s << errorStr<< Qt::endl;
 
-        } else if (readCount == 0) {
+        //parser.showHelp(1);
+        exit(1);
+
+        } else if (readCountBoot == 0) {
             //return;
+        cerr << u"Error open bootload - null length\n"_s <<bootloaderFile<<  Qt::endl;
         parser.showHelp(1);
+        exit(1);
 
         }
-	cout << u"Two files downloaded\n"_s << Qt::endl;
+        auto readCountProg{ fileMainload.read(programTemp, sizeof(programTemp)) };
+        if (readCountProg == -1) {
+            errorStr = fileMainload.errorString();
+            //emit finished(false);
+            //return;
+        cerr << u"Error open bootload \n"_s << errorStr<< Qt::endl;
+        //parser.showHelp(1);
+        exit(1);
+
+        } else if (readCountProg == 0) {
+            //return;
+        cerr << u"Error open file main program - null length \n"_s <<MainloadFile<<  Qt::endl;
+        //parser.showHelp(1);
+        exit(1);
+
+        }
+        if (readCountProg>(238*1024)){
+          cerr << u"Length of program to big: \n"_s << readCountProg<< Qt::endl;
+          //parser.showHelp(1);
+        exit(1);
+
+        }
+
+        if ((readCountBoot-FLASH_APP_START_ADDR-readCountProg)<=0){
+          cerr << u"Length of bootloader to small: \n"_s << bootloaderFile<< u":"_s <<readCountBoot<< Qt::endl;
+          //parser.showHelp(1);
+         exit(1);
+
+        }
+        //надо подсчитать контрольную сумму programTemp длиной readCountProg
+         //перед подсчётом надо:
+        // смещение 49*4 предварительно заполнить FF (хотя они там и так должны, а если не 0xFF то обругаться и ничего не делать) и туда записать crc в конце все процедуры
+	if ((unsigned char)programTemp[CRC_MAIN_PROGRAM]!=0xFF or (unsigned char)programTemp[CRC_MAIN_PROGRAM+1]!=0xFF or (unsigned char)programTemp[CRC_MAIN_PROGRAM+2]!=0xFF or (unsigned char)programTemp[CRC_MAIN_PROGRAM+3]!=0xFF){
+          cerr << u"In program crc place not 0xFFFFFFFF. Please check: may be wrong filename  \n"_s << MainloadFile << u"\nAddress crc place:\n"_s << Qt::hex << CRC_MAIN_PROGRAM;
+          cerr << u"\nReading:\n0x"_s;
+          cerr << Qt::hex <<(unsigned char)programTemp[CRC_MAIN_PROGRAM+3];
+          cerr << Qt::hex <<(unsigned char)programTemp[CRC_MAIN_PROGRAM+2];
+          cerr << Qt::hex <<(unsigned char)programTemp[CRC_MAIN_PROGRAM+1];
+          cerr << Qt::hex <<(unsigned char)programTemp[CRC_MAIN_PROGRAM+0];
+          cerr << Qt::endl;
+          //parser.showHelp(1);
+         exit(1);
+	}
+        // в смещение 50*4  lenfgth_program  длина прошивки загруженной, то есть число readCountProg
+	if ((unsigned char) programTemp[LENGTH_MAIN_PROGRAM]!=0xFF or (unsigned char)programTemp[LENGTH_MAIN_PROGRAM+1]!=0xFF or (unsigned char)programTemp[LENGTH_MAIN_PROGRAM+2]!=0xFF or (unsigned char)programTemp[LENGTH_MAIN_PROGRAM+3]!=0xFF){
+          cerr << u"In program length place not 0xFFFFFFFF. Please check: may be wrong filename  \n"_s << MainloadFile;
+          cerr << u"\nAddress LENGTH_MAIN_PROGRAM place:\n"_s << Qt::hex << LENGTH_MAIN_PROGRAM;
+          cerr << u"\nReading:\n0x"_s;
+          cerr << Qt::hex <<(unsigned char)programTemp[LENGTH_MAIN_PROGRAM+3];
+          cerr << Qt::hex <<(unsigned char)programTemp[LENGTH_MAIN_PROGRAM+2];
+          cerr << Qt::hex <<(unsigned char)programTemp[LENGTH_MAIN_PROGRAM+1];
+          cerr << Qt::hex <<(unsigned char)programTemp[LENGTH_MAIN_PROGRAM+0];
+          cerr << Qt::endl;
+          //parser.showHelp(1);
+         exit(1);
+	}
+        for (int i{ 0 }; i < 4; ++i){
+            programTemp[LENGTH_MAIN_PROGRAM+i] = (readCountProg >> 8 * i) & 0xFF;
+        }
+
+        QString pathFile="flashing.bin";
+        bool fileExists = QFileInfo::exists(pathFile) && QFileInfo(pathFile).isFile();
+        if (fileExists){
+          cerr << u"File for write exists, please remove: \n"_s << pathFile<< Qt::endl;
+          //parser.showHelp(1);
+         exit(1);
+        }
+        QFile fileTogether(pathFile);
+        fileTogether.open(QIODevice::WriteOnly);
+
+        //QByteArray byteArray;
+        //QDataStream outDataStream{ &byteArray, QIODeviceBase::WriteOnly };
+        QDataStream outDataStream(&fileTogether);
+
+        outDataStream.writeRawData(bootloaderTemp, FLASH_APP_START_ADDR); //берём с бутлоадреса все данные первой страницы, там серийный номер и таблица старта
+        outDataStream.writeRawData(programTemp, readCountProg); //в середину заталкиваем прошивку с контрольной суммой и длиной
+        outDataStream.writeRawData(bootloaderTemp, readCountBoot-FLASH_APP_START_ADDR-readCountProg); //в хвост записываем всё что осталось
+        fileTogether.close();
+
+	cout << u"Two files downloaded && concatenated:"_s <<pathFile<< Qt::endl;
+        exit(0);
 
     }
 
